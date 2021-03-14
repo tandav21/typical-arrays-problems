828881004adf4fa8a99e13ce47fbf4bd4ac2e237
tandav21 <tvdavyd@gmail.com>
2021-03-15 00:18:50 +0300

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array === null || array.length == 0) {
+    return 0;
+  }
+  else {
+    let minResult = Math.min.apply(null, array);
+    return minResult;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array === null || array.length == 0) {
+    return 0;
+  }
+  else {
+    let maxResult = Math.max.apply(null, array);
+    return maxResult;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array === null || array.length == 0) {
+    return 0;
+  }
+  else {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / array.length; 
+  }
 }
